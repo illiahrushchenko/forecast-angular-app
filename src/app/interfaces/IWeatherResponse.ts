af0ff0590bc03a2d03abbaa0c10914b012b7cb76
Illia Hrushchenko <gr.illya13@gmail.com>
2024-10-29 19:12:17 +0200

    Add api data interface

---
+++ src/app/interfaces/IWeatherResponse.ts
@@ -0,0 +1,22 @@
+export interface IWeatherResponse {
+    address: string
+    timezone: string
+    resolvedAddress: string
+    days: IWeatherResponseDay[]
+}
+
+export interface IWeatherResponseDay {
+    datetime: string
+    dayName: string
+
+    tempmax: number
+    tempmin: number
+    temp: number
+    feelslike: number
+
+    humidity: number
+    precipcover: number
+    windspeed: number
+    pressure: number
+    icon: string
+}
