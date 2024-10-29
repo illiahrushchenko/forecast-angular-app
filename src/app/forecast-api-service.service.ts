43140d47f494a11be7dd3b0d7d1d9e342a9adac2
Illia Hrushchenko <gr.illya13@gmail.com>
2024-10-29 19:17:59 +0200

    Add api service

---
+++ src/app/forecast-api-service.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IWeatherResponse } from './interfaces/IWeatherResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForecastApiService {
+
+  constructor(
+    private _http: HttpClient) { }
+
+  getDays(days: number): Observable<IWeatherResponse> {
+      let startDate = new Date();
+      let endDate = new Date();
+      endDate.setDate(startDate.getDate() + days);
+
+      let startDateString = this.parseDate(startDate);
+      let endDateString = this.parseDate(endDate);
+      return this.getData(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Vinnytsia/${startDateString}/${endDateString}`);
+  }
+
+  parseDate(date: Date): string {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+
+      return `${year}-${month}-${day}`;
+  }
+
+  private getData(url: string): Observable<IWeatherResponse>{
+      return this._http.get<IWeatherResponse>(url, {
+          params: new HttpParams()
+            .set('unitGroup', 'metric')
+            .set('include', 'days')
+            .set('key', '{key}')
+            .set('contentType', 'json')
+        });
+  }
+}
